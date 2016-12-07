7f23c19a288d82e7dfc8e909b59e1dcaf691d487
jtpal <palmerj@sonoma.edu>
2016-12-07 12:10:40 -0800

    lab12 in progress

--- model/account_dal.js
+++ model/account_dal.js
@@ -57,20 +57,6 @@ exports.update = function(params, callback) {
     });
 };
 
-/*  Stored procedure used in this example
- DROP PROCEDURE IF EXISTS school_getinfo;
- DELIMITER //
- CREATE PROCEDURE school_getinfo (school_id int)
- BEGIN
- SELECT * FROM school WHERE school_id = school_id;
- SELECT a.*, school_id FROM address a
- LEFT JOIN school s on s.address_id = a.address_id;
- END //
- DELIMITER ;
- # Call the Stored Procedure
- CALL school_getinfo (4);
- */
-
 exports.edit = function(account_id, callback) {
     var query = 'CALL account_getinfo(?)';
     var queryData = [account_id];
