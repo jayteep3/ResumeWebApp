098ad85de81c71ff983e943c66baf5a21fde2591
JT Palmer <palmerj@sonoma.edu>
2016-12-06 23:20:09 -0800

    working on lab12

--- routes/company_routes.js
+++ routes/company_routes.js
@@ -7,6 +7,7 @@ var router = express.Router();
 var company_dal = require('../model/company_dal');
 var address_dal = require('../model/address_dal');
 
+
 // View All companys
 router.get('/all', function(req, res) {
     company_dal.getAll(function(err, result){
@@ -39,8 +40,9 @@ router.get('/', function(req, res){
 
 // Return the add a new company form
 router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
     address_dal.getAll(function(err,result) {
-        if(err) {
+        if (err) {
             res.send(err);
         }
         else {
@@ -49,56 +51,59 @@ router.get('/add', function(req, res){
     });
 });
 
-// insert a company record
+// View the company for the given id
 router.get('/insert', function(req, res){
     // simple validation
     if(req.query.company_name == null) {
         res.send('Company Name must be provided.');
     }
     else if(req.query.address_id == null) {
-        res.send('Need to select at least 1 address');
+        res.send('At least one address must be selected');
     }
     else {
         // passing all the query parameters (req.query) to the insert function instead of each individually
         company_dal.insert(req.query, function(err,result) {
             if (err) {
+                console.log(err)
                 res.send(err);
             }
             else {
-                //poor practice, but we will handle it differently once we start using Ajax
+                //poor practice for redirecting the user to a different page, but we will handle it differently once we start using Ajax
                 res.redirect(302, '/company/all');
             }
         });
     }
 });
-//edit a record
-router.get('/edit', function(req,res){
-    if(req.query.company_id == null){
-        res.send('Need a company ID');
+
+router.get('/edit', function(req, res){
+    if(req.query.company_id == null) {
+        res.send('A company id is required');
     }
     else {
         company_dal.edit(req.query.company_id, function(err, result){
+            console.log(result);
             res.render('company/companyUpdate', {company: result[0][0], address: result[1]});
         });
     }
+
 });
 
-router.get('/edit2', function(req,res){
+router.get('/edit2', function(req, res){
     if(req.query.company_id == null) {
-        res.send('Need a company ID');
+        res.send('A company id is required');
     }
     else {
         company_dal.getById(req.query.company_id, function(err, company){
             address_dal.getAll(function(err, address) {
-                res.render('/company/companyUpdate', {company: company[0], address: address});
+                res.render('company/companyUpdate', {company: company[0], address: address});
             });
         });
     }
+
 });
 
-//update a company
-router.get('/update', function(req, res){
-    company_dal.update(req.query, function(err,result){
+router.get('/update', function(req, res) {
+    company_dal.update(req.query, function(err, result){
         res.redirect(302, '/company/all');
     });
 });
@@ -120,6 +125,5 @@ router.get('/delete', function(req, res){
         });
     }
 });
-
 
 module.exports = router;
